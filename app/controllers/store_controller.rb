746989281dfd7515fc5e05964d0f3bdc3b5279cb
Juan José Rodríguez López <J2RGEZ@gmail.com>
2017-11-29 16:51:38 +0000

    Add a basic cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
       @products = Product.order(:title)
+      @counter = counter_manager
+  end
+
+  private
+  def counter_manager
+      if session[:count].nil?
+          session[:count] = 1
+      else
+          session[:count] += 1
+      end
   end
 end
